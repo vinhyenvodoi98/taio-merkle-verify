f935ec1a5998bacfb89796539d3ddd86e8fc3681
vinhyenvodoi98 <vinhyenvodoi98@gmail.com>
2025-06-08 22:40:25 +0900

    feat: update swagger URL

--- src/lib/swagger.ts
+++ src/lib/swagger.ts
@@ -10,7 +10,7 @@ const options: swaggerJsdoc.Options = {
     },
     servers: [
       {
-        url: 'http://localhost:3000',
+        url: process.env.NEXT_PUBLIC_API_URL,
         description: 'Development server',
       },
     ],
